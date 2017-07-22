b9e06fe143f10b48f9eca6cc37a8dce2cd47173d
cameronwlewis <cameronwlewis@gmail.com>
2017-07-21 23:18:35 -0600

    successfully connecting and receiving data from Spotify API!

--- dist/public/javascripts/test.js
+++ dist/public/javascripts/test.js
@@ -1,10 +1,12 @@
 "use strict";
 
+var accessToken = '';
+
 function data_test() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            document.getElementById("demo").innerHTML = this.responseText;
+            accessToken = this.responseText;
         }
     };
     xhttp.open("GET", "http://localhost:3000/data_test", true);
@@ -12,4 +14,19 @@ function data_test() {
 }
 
 // put code here that searches playlists, and see if it works!
+
+
+function testSpotify() {
+    var spotify = new SpotifyWebApi();
+    spotify.setAccessToken(accessToken);
+
+    spotify.searchPlaylists('pop', { limit: 1 }, function (err, data) {
+
+        if (err) console.error(err);else {
+            console.log('Pop Playlist: ', data);
+            var playlist = data.playlists.items[0].name;
+            console.log(playlist);
+        }
+    });
+}
 //# sourceMappingURL=test.js.map
--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,8 +1,11 @@
+
+var accessToken ='';
+
 function data_test() {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            document.getElementById("demo").innerHTML = this.responseText;
+            accessToken = this.responseText;
         }
     };
     xhttp.open("GET", "http://localhost:3000/data_test", true);
@@ -10,4 +13,20 @@ function data_test() {
 }
 
 // put code here that searches playlists, and see if it works!
+
+
+function testSpotify() {
+    var spotify = new SpotifyWebApi();
+    spotify.setAccessToken(accessToken);
+
+    spotify.searchPlaylists('pop', {limit: 1}, function (err, data) {
+
+        if (err) console.error(err);
+        else {
+            console.log('Pop Playlist: ', data);
+            var playlist = data.playlists.items[0].name;
+            console.log(playlist);
+        }
+    });
+}
 
