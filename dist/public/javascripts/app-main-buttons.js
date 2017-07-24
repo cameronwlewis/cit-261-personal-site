306f933bef803a8f5f57a8ce45708699856736a4
cameronwlewis <cameronwlewis@gmail.com>
2017-07-24 17:28:19 -0600

    image uploader button correctly functioning now

--- dist/public/javascripts/app-main-buttons.js
+++ dist/public/javascripts/app-main-buttons.js
@@ -2,14 +2,14 @@
 
 function chooserButton_in() {
     //document.getElementById('btn-border').style.stroke = '#EA3126';
-    document.getElementById('image-chooser-button-bg').style.fill = '#76BED0';
-    document.getElementById('image-chooser-text').style.fill = '#ffffff';
+    document.getElementById('imageLoader-bg').style.fill = '#76BED0';
+    document.getElementById('imageLoader-text').style.fill = '#ffffff';
 }
 
 function chooserButton_out() {
     // document.getElementById('btn-border').style.stroke = '#76BED0';
-    document.getElementById('image-chooser-button-bg').style.fill = '#ffffff';
-    document.getElementById('image-chooser-text').style.fill = '#76BED0';
+    document.getElementById('imageLoader-bg').style.fill = '#ffffff';
+    document.getElementById('imageLoader-text').style.fill = '#76BED0';
 }
 function submitButton_in() {
     //document.getElementById('btn-border').style.stroke = '#EA3126';
@@ -23,13 +23,13 @@ function submitButton_out() {
     document.getElementById('submit-image-text').style.fill = '#76BED0';
 }
 
-document.getElementById('image-chooser-button').addEventListener("mouseover", chooserButton_in);
-document.getElementById('image-chooser-button').addEventListener("mouseout", chooserButton_out);
+document.getElementById('imageLoader').addEventListener("mouseover", chooserButton_in);
+document.getElementById('imageLoader').addEventListener("mouseout", chooserButton_out);
 document.getElementById('submit-image-button').addEventListener("mouseover", submitButton_in);
 document.getElementById('submit-image-button').addEventListener("mouseout", submitButton_out);
 
-document.getElementById('image-chooser-button').addEventListener("touchstart", chooserButton_in);
-document.getElementById('image-chooser-button').addEventListener("touchend", chooserButton_out);
+document.getElementById('imageLoader').addEventListener("touchstart", chooserButton_in);
+document.getElementById('imageLoader').addEventListener("touchend", chooserButton_out);
 document.getElementById('submit-image-button').addEventListener("touchstart", submitButton_in);
 document.getElementById('submit-image-button').addEventListener("touchend", submitButton_out);
 //# sourceMappingURL=app-main-buttons.js.map
--- public/javascripts/app-main-buttons.js
+++ public/javascripts/app-main-buttons.js
@@ -1,13 +1,13 @@
 function chooserButton_in() {
     //document.getElementById('btn-border').style.stroke = '#EA3126';
-    document.getElementById('image-chooser-button-bg').style.fill = '#76BED0';
-    document.getElementById('image-chooser-text').style.fill = '#ffffff';
+    document.getElementById('imageLoader-bg').style.fill = '#76BED0';
+    document.getElementById('imageLoader-text').style.fill = '#ffffff';
 }
 
 function chooserButton_out() {
     // document.getElementById('btn-border').style.stroke = '#76BED0';
-    document.getElementById('image-chooser-button-bg').style.fill = '#ffffff';
-    document.getElementById('image-chooser-text').style.fill = '#76BED0';
+    document.getElementById('imageLoader-bg').style.fill = '#ffffff';
+    document.getElementById('imageLoader-text').style.fill = '#76BED0';
 }
 function submitButton_in() {
     //document.getElementById('btn-border').style.stroke = '#EA3126';
@@ -21,12 +21,12 @@ function submitButton_out() {
     document.getElementById('submit-image-text').style.fill = '#76BED0';
 }
 
-document.getElementById('image-chooser-button').addEventListener("mouseover", chooserButton_in);
-document.getElementById('image-chooser-button').addEventListener("mouseout", chooserButton_out);
+document.getElementById('imageLoader').addEventListener("mouseover", chooserButton_in);
+document.getElementById('imageLoader').addEventListener("mouseout", chooserButton_out);
 document.getElementById('submit-image-button').addEventListener("mouseover", submitButton_in);
 document.getElementById('submit-image-button').addEventListener("mouseout", submitButton_out);
 
-document.getElementById('image-chooser-button').addEventListener("touchstart", chooserButton_in);
-document.getElementById('image-chooser-button').addEventListener("touchend", chooserButton_out);
+document.getElementById('imageLoader').addEventListener("touchstart", chooserButton_in);
+document.getElementById('imageLoader').addEventListener("touchend", chooserButton_out);
 document.getElementById('submit-image-button').addEventListener("touchstart", submitButton_in);
 document.getElementById('submit-image-button').addEventListener("touchend", submitButton_out);
